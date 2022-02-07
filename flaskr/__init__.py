bd81b96ff47e41eeab09b7aa49de9d9c9eff5ed4
Máirtín Ó Flatharta <mairtinflatherty2014@gmail.com>
2022-02-07 19:59:16 +0000

    Implement user session routes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,31 +1,76 @@
-import os
+import json
+from flask import Flask, render_template_string
+from flask_mongoengine import MongoEngine
+from flask_user import login_required, UserManager, UserMixin
 
-from flask import Flask
 
+def create_app():
+    """ Flask application factory """
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-    )
+    # Setup Flask and load app.config
+    app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # Config is external JSON file for now. Better than commiting keys
+    app.config.from_json('../../application.json')
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # Setup Flask-MongoEngine
+    db = MongoEngine(app)
 
-    # test page that prints test message
+    # Define the User document.
+    class User(db.Document, UserMixin):
+        active = db.BooleanField(default=True)
+
+        # User authentication information
+        username = db.StringField(default='')
+        password = db.StringField()
+
+        # User information
+        first_name = db.StringField(default='')
+        last_name = db.StringField(default='')
+
+        # Relationships
+        roles = db.ListField(db.StringField(), default=[])
+
+    # Setup Flask-User and specify the User data-model
+    user_manager = UserManager(app, db, User)
+
+    # The Home page is accessible to anyone
     @app.route('/')
-    def hello():
-        return 'Test out'
+    def home_page():
+        # String-based templates
+        return render_template_string("""
+            {% extends "flask_user_layout.html" %}
+            {% block content %}
+                <h2>Home page</h2>
+                <p><a href={{ url_for('user.register') }}>Register</a></p>
+                <p><a href={{ url_for('user.login') }}>Sign in</a></p>
+                <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
+                <p><a href={{ url_for('data_page') }}>Data page</a> (login required)</p>
+                <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
+            {% endblock %}
+            """)
+
+    # The Data page is only accessible to authenticated users via the @login_required decorator
+    @app.route('/data')
+    @login_required    # User must be authenticated
+    def data_page():
+        # String-based templates
+        return render_template_string("""
+            {% extends "flask_user_layout.html" %}
+            {% block content %}
+                <h2>Members page</h2>
+                <p><a href={{ url_for('user.register') }}>Register</a></p>
+                <p><a href={{ url_for('user.login') }}>Sign in</a></p>
+                <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
+                <p><a href={{ url_for('data_page') }}>Member page</a> (login required)</p>
+                <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
+            {% endblock %}
+            """)
 
     return app
+
+
+# Start server
+if __name__=='__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
